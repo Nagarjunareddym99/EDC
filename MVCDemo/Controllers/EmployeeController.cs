c5cc318ecf4c09550fc79dd253b24743ea01c1c1
NagarjunaReddy <nagarjunareddy.m99@outlook.com>
2023-04-18 18:27:53 +0530

    a

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+           List<Employee> employees = employeeContext.Employees.ToList();
+
+            return View(employees);
+        }
+    
+        public ActionResult GetDetails(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee =  employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+            
+            return View(employee);
+        }
+    }
+}
